6f77292cba0c47ea3414312f6b199da71d617f19
nhelvig <nhelvig@berkeley.edu>
2015-08-27 12:57:02 -0700

    Exercise 2 complete.

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+
+    int count;
+
+    public Accumulator() {
+        this.count = 0;
+    }
+    public void increment() {
+        this.count += 1;
+    }
+
+    public void total() {
+        System.out.println(this.count);
+    }
 }
