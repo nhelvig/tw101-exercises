f1b2af7343e187e265503c8d652ee65e4d93b2a6
nhelvig <nhelvig@berkeley.edu>
2015-08-31 16:59:33 -0700

    DiamondExercises complete.

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.tw101.introductory_programming_exercises;
 
+import java.util.ArrayList;
+
 public class DiamondExercises {
     public static void main(String[] args) {
         drawAnIsoscelesTriangle(3);
@@ -14,6 +16,18 @@ public class DiamondExercises {
 //            *****
     private static void drawAnIsoscelesTriangle(int n) {
 
+        for (int total = 0; total < n; total++) {
+            String spaces = "";
+            String stars = "";
+            for (int i = total; i < n - 1; i++) {
+                spaces += " ";
+            }
+            for (int star = 0; star < (total * 2) + 1; star++) {
+                stars += "*";
+            }
+            String finalLine = spaces + stars + spaces;
+            System.out.println(finalLine);
+        }
     }
 
 //    Diamond
@@ -24,7 +38,25 @@ public class DiamondExercises {
 //             ***
 //              *
     private static void drawADiamond(int n) {
-
+        ArrayList<String> rows = new ArrayList(n);
+        for (int total = 0; total < n; total++) {
+            String spaces = "";
+            String stars = "";
+            for (int i = total; i < n - 1; i++) {
+                spaces += " ";
+            }
+            for (int star = 0; star < (total * 2) + 1; star++) {
+                stars += "*";
+            }
+            String finalLine = spaces + stars + spaces;
+            rows.add(total, finalLine);
+        }
+        for (String row : rows) {
+            System.out.println(row);
+        }
+        for (int i = rows.size() - 2; i >= 0; i--) {
+            System.out.println(rows.get(i));
+        }
     }
 
 //    Diamond with Name
@@ -36,6 +68,25 @@ public class DiamondExercises {
 //            ***
 //             *
     private static void drawADiamondWithYourName(int n) {
-
+        ArrayList<String> rows = new ArrayList(n);
+        for (int total = 0; total < n; total++) {
+            String spaces = "";
+            String stars = "";
+            for (int space = total; space < n - 1; space++) {
+                spaces += " ";
+            }
+            for (int star = 0; star < (total * 2) + 1; star++) {
+                stars += "*";
+            }
+            String finalLine = spaces + stars + spaces;
+            rows.add(total, finalLine);
+        }
+        for (int i = 0; i < rows.size() - 1; i++) {
+            System.out.println(rows.get(i));
+        }
+        System.out.println("Nicolas");
+        for (int i = rows.size() - 2; i >= 0; i--) {
+            System.out.println(rows.get(i));
+        }
     }
 }
