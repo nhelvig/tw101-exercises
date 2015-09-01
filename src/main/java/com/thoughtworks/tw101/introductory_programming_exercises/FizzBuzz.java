124e333ff2fb700cd518d4f4662f0960aa9afbae
nhelvig <nhelvig@berkeley.edu>
2015-08-31 17:02:51 -0700

    FizzBuzz complete.

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
@@ -32,6 +32,16 @@ public class FizzBuzz {
     }
 
     private static void fizzBuzz() {
-
+        for (int number = 1; number <= 100; number++) {
+            if (number % 3 == 0 && number % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (number % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (number % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(number);
+            }
+        }
     }
 }
