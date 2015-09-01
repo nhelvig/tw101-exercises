afc20171ea6da1b09c727269afa5861ad40db351
nhelvig <nhelvig@berkeley.edu>
2015-08-31 17:22:21 -0700

    TriangleExercises complete.

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/TriangleExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/TriangleExercises.java
@@ -2,6 +2,9 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 
 public class TriangleExercises {
     public static void main(String[] args) {
+        //The functions all work as intended on their own. However, if you combine them they
+        //will overlap because I used print() instead of println(). I did not want to add in
+        //extra println() lines because then the functions would not work independently.
         drawAnAsterisk();
         drawAHorizontalLine(8);
         drawAVerticalLine(3);
@@ -12,14 +15,16 @@ public class TriangleExercises {
 //    Print one asterisk to the console.
 //    Example: *
     private static void drawAnAsterisk() {
-
+        System.out.print("*");
     }
 
 //    Draw a horizontal line
 //    Given a number n, print n asterisks on one line.
 //    Example when n=8:  ********
     private static void drawAHorizontalLine(int n) {
-
+        for (int i = 0; i < n; i++) {
+            drawAnAsterisk();
+        }
     }
 
 //    Draw a vertical line
@@ -29,7 +34,10 @@ public class TriangleExercises {
 //            *
 //            *
     private static void drawAVerticalLine(int n) {
-
+        for (int i = 0; i < n; i++) {
+            drawAnAsterisk();
+            System.out.println();
+        }
     }
 
 //    Draw a right triangle
@@ -39,6 +47,11 @@ public class TriangleExercises {
 //            **
 //            ***
     private static void drawARightTriangle(int n) {
-
+        for (int i = 1; i <= n; i++) {
+            for (int x = i; x > 0; x--) {
+                drawAnAsterisk();
+            }
+            System.out.println();
+        }
     }
 }
